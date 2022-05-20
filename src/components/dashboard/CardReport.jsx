a0c088ec17b4d7fde4b26f5ad20dd7b4e120dc41
leonardo <leonardosantos45548@gmail.com>
2022-05-20 01:02:37 -0300

    correction divs

--- src/components/dashboard/CardReport.jsx
+++ src/components/dashboard/CardReport.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import ApiFinance from "../../services/ApiFinance"
-import FieldsReport from "./FieldsReport";
 
+import FieldsReport from "./FieldsReport";
+import Wallet from "../../assets/wallet.svg";
 import "./CardReport.css";
 
 const CardReport = props => {
@@ -9,28 +9,38 @@ const CardReport = props => {
     const balance = props.accountBalance ? props.accountBalance : "-";
 
     return (
-        <div className="Card">
+
+        <div className="CardDashboard">
+            <div className="Box-img">
+                <img src={Wallet} alt="wallet" />
+            </div>
+
             <FieldsReport
                 className="Box-saldo"
                 title="Saldo em conta"
                 value={balance}
             />
+
             <FieldsReport
                 className="Box-receita"
                 title="Receita"
                 value={props.recipe}
             />
+
             <FieldsReport
                 className="Box-despesa"
                 title="Despesa"
                 value={props.cost}
             />
+
             <div className="Text-bottom">
                 <p><span>R$55</span> próxima despesa -
                     <b> 03/05/22</b>
                 </p>
             </div>
+
         </div>
+
     )
 }
 
