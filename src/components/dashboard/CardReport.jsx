ed81c6b8373e1699cdd6885524855b2dfa5aa7e2
leonardo <leonardosantos45548@gmail.com>
2022-05-19 23:24:08 -0300

    card report

--- src/components/dashboard/CardReport.jsx
+++ src/components/dashboard/CardReport.jsx
@@ -6,39 +6,32 @@ import "./CardReport.css";
 
 const CardReport = props => {
 
-    const balance = props.accountBalance? props.accountBalance : "-";
+    const balance = props.accountBalance ? props.accountBalance : "-";
 
     return (
-        <div className="Card-report">
-            <div className="Card">
-                <FieldsReport
-                    className="Box-saldo"
-                    title="Saldo em conta"
-                    value={balance}
-                />
-                <FieldsReport
-                    className="Box-receita"
-                    title="Receita"
-                    value={props.recipe}
-                />
-                <FieldsReport
-                    className="Box-despesa"
-                    title="Despesa"
-                    value={props.cost}
-                />
-                <div className="Text-bottom">
-                    <p><span>R$55</span> próxima despesa -
-                        <b> 03/05/22</b>
-                    </p>
-                </div>
+        <div className="Card">
+            <FieldsReport
+                className="Box-saldo"
+                title="Saldo em conta"
+                value={balance}
+            />
+            <FieldsReport
+                className="Box-receita"
+                title="Receita"
+                value={props.recipe}
+            />
+            <FieldsReport
+                className="Box-despesa"
+                title="Despesa"
+                value={props.cost}
+            />
+            <div className="Text-bottom">
+                <p><span>R$55</span> próxima despesa -
+                    <b> 03/05/22</b>
+                </p>
             </div>
         </div>
-
     )
 }
-
-
-
-
 
 export default CardReport;
