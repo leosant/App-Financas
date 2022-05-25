8a01a286eea6b7c845b335e74365a0601123f05b
leonardo <leonardosantos45548@gmail.com>
2022-05-25 00:24:15 -0300

    new transtion in component of include account

--- src/pages/CreateWallet.jsx
+++ src/pages/CreateWallet.jsx
@@ -7,10 +7,21 @@ const CreateWallet = props => {
 
     return (
         <form className='Form-account'>
+            
             <h2>Adicionar nova conta</h2>
-            <input id='Insert-wallet01' placeholder='Nome da conta' />
-            <input id='Insert-wallet02' placeholder='Valor inicial' />
-            <div>
+
+            <div className='Form-inputs'>
+                <input autoComplete='off' required type='text'
+                name='name' />
+                <span>Nome da conta</span>
+            </div>
+            <div className='Form-inputs'>
+                <input autoComplete='off' required type='number'
+                name='name' />
+                <span>Valor inicial</span>
+            </div>
+
+            <div className='Form-radios'>
                 <input type='radio'></input>
                 <input type='radio'></input>
                 <input type='radio'></input>
@@ -18,10 +29,14 @@ const CreateWallet = props => {
                 <input type='radio'></input>
                 <label>Cor da conta</label>
             </div>
-            <label class="switch">
-                <input type="checkbox" />
-                <span class="slider">Visível</span>
-            </label>
+
+            <div>
+                <label class="switch">
+                    <input type="checkbox" />
+                    <span class="slider">Visível</span>
+                </label>
+            </div>
+
             <button>Adicionar</button>
         </form>
     );
